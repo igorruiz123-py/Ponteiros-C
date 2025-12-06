f5d7ffca4783160ba10d937d198b1542530e04c1
Ruiz <IRUIZ1@ball.com>
2025-12-06 13:04:00 -0300

    Adicionando as primeiras pastas do diretório

---
+++ ESTUDOS/aula1.c
@@ -0,0 +1,15 @@
+
+// Endereço de memória
+/*
+Cada variável de um programa ocupa um certo número de bytes consecutivos na memória do computador. Uma variável do tipo char ocupa 1 byte.  
+Uma variável do tipo int ocupa 4 bytes e um double ocupa 8 bytes em muitos computadores. 
+O número exato de bytes de uma variável é dado pelo operador sizeof. 
+A expressão sizeof (char), por exemplo, vale 1 em todos os computadores e a expressão sizeof (int) vale 4 em muitos computadores.
+*/
+
+/*
+Um ponteiro (= apontador = pointer) é um tipo especial de variável que armazena um endereço
+Se um ponteiro p armazena o endereço de uma variável i, podemos dizer  p aponta para i  ou  p é o endereço de i.  
+(Em termos um pouco mais abstratos, diz-se que p é uma referência à variável i.)  Se um ponteiro p tem valor diferente de NULL então *p
+*/
+
